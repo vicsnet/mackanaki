8f2dd640e5b657bd07c63877fdd3ca02cfb98c1c
Kalu Patrick Okechukwu <kalupatrick94@gmail.com>
2022-09-20 13:55:30 +0100

    auth 03

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -11,10 +11,15 @@ export default class AuthService extends BaseService {
   }
 
   async login(loginData: { [props: string]: string }) {
+    // const extraBody = {
+    //   grant_type: "password",
+    //   client_id: "2",
+    //   client_secret: "diG5UvJ8uJV7qB96IUaReysbl0wmlu0qoQG3BSUb",
+    // };
     const extraBody = {
       grant_type: "password",
-      client_id: "2",
-      client_secret: "diG5UvJ8uJV7qB96IUaReysbl0wmlu0qoQG3BSUb",
+      client_id: "14",
+      client_secret: "SF23V7oON8sVS1gIRvubdoBaWPr7rjkREATckkmJ",
     };
     const data = { ...loginData, ...extraBody };
     const response = await axios.post(this.BASE_URL + "/oauth/token", data);
