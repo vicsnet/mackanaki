21b6d97ee3b0038068451de37673b200a97481bd
Kalu Patrick Okechukwu <kalupatrick94@gmail.com>
2022-09-30 08:03:50 +0100

    login fix

--- src/pages/Authentication/Login.tsx
+++ src/pages/Authentication/Login.tsx
@@ -10,6 +10,8 @@ import Loader from 'react-loader-advanced';
 import { ScaleLoader } from 'react-spinners';
 import { useAppDispatch, useAppSelector } from '../../redux/app/hooks';
 import { getLoginState, loginUser, resetState } from '../../redux/features/authentication/loginSlice';
+import { authenicateWithGoogle, getGoogleAuthState } from '../../redux/features/authentication/googleAuthSlice';
+import { toast } from 'react-toastify';
 
 
 
@@ -30,6 +32,7 @@ const Login = () => {
     };
 
     const { error, status } = useAppSelector(getLoginState);
+    const { status: googleStatus, error: googleErr, target_url } = useAppSelector(getGoogleAuthState);
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
@@ -50,6 +53,17 @@ const Login = () => {
 
 
     useEffect(() => {
+        if (googleStatus === "failed") {
+            toast.error(googleErr, {
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        }
+        dispatch(authenicateWithGoogle());
         if (status === "failed") {
             form.customToast({ type: "error", message: error });
             dispatch(resetState());
@@ -57,14 +71,14 @@ const Login = () => {
             navigate('/');
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [status]);
+    }, [status, googleErr]);
 
 
     return (
         <Fragment>
             <PageLayout>
                 {/* LOGIN CONTAINER START */}
-                <Loader show={status === "loading"} backgroundStyle={{ background: "none" }} message={<ScaleLoader cssOverride={override} color="#c1c1c1" />}>
+                <Loader show={status === "loading" || googleStatus === "loading"} backgroundStyle={{ background: "none" }} message={<ScaleLoader cssOverride={override} color="#c1c1c1" />}>
                     <div className="flex justify-center items-center p-6">
                         <div className="rounded-3xl shadow-lg shadow-zinc-900 bg-secondaryColor h-auto py-10 md:px-4">
                             <div className="flex flex-col justify-center items-center gap-2">
@@ -108,6 +122,13 @@ const Login = () => {
                                             </Link>
                                             </p>
                                             <Button name="Login" disabled={isvalidForm} className="mx-auto" />
+                                        </div>
+                                        <p className="mt-7 text-signupTextColor before:content-['-----------------'] before:border-b-signupTextColor after:content-['----------------'] after:border-b-signupTextColor mx-auto md:text-sm text-xs">OR</p>
+                                        <div className="flex mt-7 flex-col justify-center items-center gap-2 mx-auto">
+                                            <p className="mx-4 text-signupTextColor md:text-sm text-xs text-center">Continue with
+                                                <span onClick={() => window.location.replace(target_url !== null ? (target_url as string) : "",)} className="ml-1 text-primaryColor underline cursor-pointer">Google</span>
+
+                                            </p>
                                         </div>
                                     </div>
                                 </form>
